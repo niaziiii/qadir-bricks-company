680e9625f96bcf37254f758d9678e7cd4b48ddd2
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-10-25 13:36:38 -0700

    loading aniamtion

---
+++ src/components/loadingAnimation/LoadingAnimation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './animation.scss'
+
+const LoadingAnimation = () => {
+    return (
+        <div className='loader-container'>
+            <h1 className='loading'>Loading</h1>
+        </div>
+    )
+}
+
+export default LoadingAnimation
